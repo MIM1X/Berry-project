e048fd866bd1b47239299e6b0507397b386957ae
MIMIX <41s41@bk.ru>
2025-05-22 14:28:30 +0300

    Выполнил оставшуюся стилизацию по ТЗ

--- source/scripts/navOpen.js
+++ source/scripts/navOpen.js
@@ -2,6 +2,9 @@ const header = document.querySelector('.header');
 const toggle = header.querySelector('.header__toggle');
 const nav = header.querySelector('.header__nav');
 
+toggle.classList.remove('header__toggle--nojs')
+nav.classList.remove('header__nav--nojs')
+
 toggle.addEventListener('click', () => {
   nav.classList.toggle('header__nav--opened');
   nav.classList.toggle('header__nav--closed');
